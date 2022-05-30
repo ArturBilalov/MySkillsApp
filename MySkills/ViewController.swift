ff08426b7b6cd8ae826337d2646d31a93d9b8bb6
ArturBilalov <art.buddha8@gmail.com>
2022-05-30 16:34:54 +0300

    Initial Commit

---
+++ MySkills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySkills
+//
+//  Created by Artur Bilalov on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
